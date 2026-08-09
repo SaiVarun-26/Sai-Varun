65d4e56a96537a84eb816dc72d67c91b41afc362
SaiVarun-26 <saivarun.ksvt@gmail.com>
2026-08-09 22:44:08 +0530

    Completed user management module

--- app/utils/security.py
+++ app/utils/security.py
@@ -1,1 +1,9 @@
-git 
+from passlib.context import CryptContext
+
+pwd_context = CryptContext(
+    schemes=["bcrypt"],
+    deprecated="auto"
+)
+
+def hash_password(password: str):
+    return pwd_context.hash(password)
